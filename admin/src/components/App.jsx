5ce58168105f6f47c9375f43120f8509f6aca7d4
cliangsuper@gmail.com <liang_eddi1989@yahoo.com>
2018-06-17 13:49:05 +1000

    Add dependency and admin template

---
+++ admin/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+
+const App = () => (
+  <Jumbotron fluid>
+    <Container fluid className="text-center">
+      <p class="lead">Welcome to Lighthouse CMS</p>
+    </Container>
+  </Jumbotron>
+);
+
+export default App;
---
+++ admin/webpack.config.js
@@ -0,0 +1,64 @@
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
+module.exports = {
+  entry: {
+    app: ['./src/index.jsx'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+        resolve: {
+          extensions: ['.js', '.jsx'],
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader?limit=100000',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+      chunkFilename: "[id].css"
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    })
+  ],
+};
