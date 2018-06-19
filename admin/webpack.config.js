3bd8abb9267dc656d64f66890bca0959481285d3
cliangsuper@gmail.com <liang_eddi1989@yahoo.com>
2018-06-20 00:29:37 +1000

    Update dist path

--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'dist/public'),
   },
   module: {
     rules: [
